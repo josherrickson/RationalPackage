ed243ddac31975787db236f5d407680049b71681
Presentation <presentation@Worf-10.local>
2024-12-03 10:16:45 -0500

    new package creation

---
+++ R/Rational Class.R
@@ -0,0 +1,27 @@
+setClass("Rational",
+         slots = c(numerator = "integer",
+                   denominator = "integer"))
+
+
+### I'm using a deliberately short and non-descriptive function name here. I
+### envision that instead of c(5, 2), you'd call R(5, 2)
+##' @title Rational number creator
+##' @param num Numerator integer
+##' @param denom Denominator integer
+##' @return A `Rational` object
+R <- function(num, denom) {
+  num <- as.integer(num)
+  denom <- as.integer(denom)
+  
+  new("Rational", numerator = num, denominator = denom)
+}
+
+setValidity("Rational", function(object) {
+  if (is.na(object@denominator) || is.na(object@numerator)) {
+    stop("Cannot contain NA")
+  }
+  if (object@denominator == 0) {
+    stop("Rational number must be finite")
+  }
+  return(TRUE)
+})
---
+++ R/Rationl Functions.R
@@ -0,0 +1,96 @@
+##' @title Show method for `Rational` object
+##' @param object A `Rational` object
+##' @return `object`, invisibly
+setMethod("show", "Rational", function(object) {
+  cat(object@numerator, "/", object@denominator, "\n")
+  return(invisible(object))
+})
+
+
+##' @title GCD and LCM functions
+##' @param x A numeric object that can be coerced to integer
+##' @param y A numeric object that can be coerced to integer
+##' @return The GCD or LCM
+##' @rdname gcd_and_lcm
+Rcpp::cppFunction("
+int gcd(int x, int y) {
+ return  std::gcd(x, y);
+}")
+
+##' @rdname gcd_and_lcm
+Rcpp::cppFunction("
+int lcm(int x, int y) {
+ return  std::lcm(x, y);
+}")
+
+##' @title Simplify a `Rational` object
+##' @param object A `Rational` object
+##' @return A `Rational` object equal to `object`, but whose numerator and
+##'   denominator have GCD of 1.
+simplify <- function(object) {
+  stopifnot(is(object, "Rational"))
+  divisor <- gcd(object@numerator, object@denominator)
+  return(R(object@numerator/divisor,
+           object@denominator/divisor))
+}
+
+##' @title Convert a `Rational` into a decimal
+##' @param object A `Rational` object
+##' @param digits The number of digits to print. The returned value has machine
+##'   precision.
+##' @return The decimal numeric created from the `Rational`
+quotient <- function(object, digits = NULL) {
+  stopifnot(is(object, "Rational"))
+  if(!(is.null(digits) ||
+       is.integer(digits) ||
+       (is.numeric(digits) & digits == round(digits) & digits > 0))) {
+    stop("digits must be a positive integer or NULL")
+  }
+  
+  return(print(object@numerator/object@denominator, digits = digits))
+}
+
+##' @title Operators for `Rational` Objects
+##' @param e1 a `Rational` Object
+##' @param e2 a `Rational` Object
+##' @return a `Rational` Object
+##' @rdname rational_ops
+setMethod("+", signature("Rational", "Rational"),
+          function(e1, e2) {
+            lcm <- lcm(e1@denominator, e2@denominator)
+            scale1 <- lcm/e1@denominator
+            scale2 <- lcm/e2@denominator
+            
+            return(R(e1@numerator*scale1 + e2@numerator*scale2,
+                     lcm))
+          })
+
+# This isn't explicitly needed, but is nice to have. One argument implies "-R()"
+# as opposed to the two argument version below for "R() - R()".
+##' @rdname rational_ops
+setMethod("-", signature("Rational"),
+          function(e1) {
+            e1@numerator <- -e1@numerator
+            return(e1)
+          })
+
+##' @rdname rational_ops
+setMethod("-", signature("Rational", "Rational"),
+          function(e1, e2) {
+            return(e1 + (-e2))
+          })
+
+
+##' @rdname rational_ops
+setMethod("*", signature("Rational", "Rational"),
+          function(e1, e2) {
+            return(R(e1@numerator*e2@numerator,
+                     e1@denominator*e2@denominator))
+          })
+
+##' @rdname rational_ops
+setMethod("/", signature("Rational", "Rational"),
+          function(e1, e2) {
+            e2 <- R(e2@numerator, e2@denominator)
+            return(e1 * e2)
+          })
